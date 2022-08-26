5da4c609d5245e20a5303c4bb44408866165faae
Carlos Escobar <escobarcarlos@gmail.com>
2022-08-26 16:25:34 -0700

    projects-finished

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,26 +1,35 @@
-/* eslint-disable react/no-unescaped-entities */ 
-import React from 'react'
-import Image from 'next/image'
-
-import createWand3D from "../public/assets/Projects/createWand3D.jpeg"
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import createWand3D from "../public/assets/Projects/createWand3D.jpeg";
+import ExpenseTracker from "../public/assets/Projects/ExpenseTracker.jpeg"
 const Projects = () => {
   return (
-    <div className='w-full'>
-        <div className='max-w-[1240px] mx-auto px-2 py-16'>
-            <p className='text-xl tracking-widest uppercase text-[#5651ef]'>Projects</p>
-            <h2>What I've built </h2>
-            <div className='grid md:grid-cols-2 gap-8'>
-                <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651ef] to-[#709dff]'>
-                     <Image className='rounded-xl group-hover:opacity-10' src={createWand3D} alt="/" />
-                     <div>
-                        <h3>Wizard Creator</h3>
-                     </div>
-                </div>
-            </div>
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651ef]">
+          Projects
+        </p>
+        <h2>What I've built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Wizard Creator"
+            backgroundImg={createWand3D}
+            projectUrl="https://one-sip-b05ef.web.app/"
+            
+
+          />
+
+          <ProjectItem
+            title="Budget Checker"
+            backgroundImg={ExpenseTracker}
+            projectUrl="/expenseTracker"
+          />
 
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
